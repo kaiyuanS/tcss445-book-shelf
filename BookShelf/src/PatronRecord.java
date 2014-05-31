535264cce9e1ef8a082e5e51cc9acddb0d70bca5
kmalex90@uw.edu <kmalex90@uw.edu@9ae6fbfb-7e5f-bf5b-6895-a5ad835334d6>
2014-05-31 20:55:31 +0000

    BookShelf upload

---
+++ BookShelf/src/PatronRecord.java
@@ -0,0 +1,146 @@
+import java.util.Calendar;
+/**
+ * The PatronRecord class holds all the information related to when a patron will check out
+ * a book.
+ * @author Kevin Alexander
+ * @version May 31, 2014
+ *
+ */
+public class PatronRecord {
+	/**
+	 * The static recordNum for the recordID.
+	 */
+	private static int recordNum = 0;
+	/**
+	 * The ID of the current record.
+	 */
+	private int recordID;
+	/**
+	 * The ID of the patron for this record.
+	 */
+	private int patronID;
+	/**
+	 * The ID of the book for this record.
+	 */
+	private int bookID;
+	/**
+	 * The date that this book was borrowed by.
+	 */
+	private Calendar borrowBy;
+	/**
+	 * The date that this book was/will be returned by.
+	 */
+	private Calendar returnBy;
+	
+	/**
+	 * The PatronRecord constructor that will be used when populating a PatronRecord with 
+	 * already existing information.
+	 * @param aRecordID The ID of the current PatronRecord.
+	 * @param aPatronID The ID of the patron for this record.
+	 * @param aBookID The ID of the book for this record.
+	 * @param aBorrowByDate The date that this book was borrowed by.
+	 * @param aReturnByDate The date that this book was/will be returned by.
+	 */
+	public PatronRecord(int aRecordID, int aPatronID, int aBookID, Calendar aBorrowByDate, 
+						Calendar aReturnByDate) {
+		recordID = aRecordID;
+		patronID = aPatronID;
+		bookID = aBookID;
+		borrowBy = aBorrowByDate;
+		returnBy = aReturnByDate;
+	}
+	
+	/**
+	 * The PatronRecord constructor which will be used when new PatronRecords need to be
+	 * made.
+	 * @param aPatronID The ID of the patron for this record.
+	 * @param aBookID The ID of the book for this record.
+	 */
+	public PatronRecord(int aPatronID, int aBookID) {
+		this(recordNum, aPatronID, aBookID, Calendar.getInstance(), null);
+		recordNum++;
+	}
+	
+	
+	/**
+	 * This method sets the recordID field to the given recordID.
+	 * @param aRecordID The new recordID.
+	 */
+	public void setRecordID(int aRecordID) {
+		recordID = aRecordID;
+	}
+	
+	/**
+	 * This method sets the patronID field to the given patronID.
+	 * @param aPatronID The new patronID.
+	 */
+	public void setPatronID(int aPatronID) {
+		patronID = aPatronID;
+	}
+	
+	/**
+	 * This method sets the bookID field to the given bookID.
+	 * @param aBookID The new bookID.
+	 */
+	public void setBookID(int aBookID) {
+		bookID = aBookID;
+	}
+	
+	/**
+	 * This method sets the borrowBy field to the new borrowBy date.
+	 * @param aBorrowByDate The new date the book was borrowed by.
+	 */
+	public void setBorrowByDate(Calendar aBorrowByDate) {
+		
+		borrowBy = aBorrowByDate;
+	}
+	
+	/**
+	 * This method sets the returnBy field to the new returnBy date.
+	 * @param aReturnByDate The new date the book was returned by.
+	 */
+	public void setReturnByDate(Calendar aReturnByDate) {
+		returnBy = aReturnByDate;
+	}
+	
+	// getters
+	/**
+	 * This method returns the recordID of the current record.
+	 * @return The recordID.
+	 */
+	public int getRecordID() {
+		return recordID;
+	}
+	
+	/**
+	 * This method returns the patronID of the current record.
+	 * @return The patronID.
+	 */
+	public int getPatronID() {
+		return patronID;
+	}
+	
+	/**
+	 * This method returns the bookID of the current record.
+	 * @return The bookID.
+	 */
+	public int getBookID() {
+		return bookID;
+	}
+	
+	/**
+	 * This method returns the borrowBy date of the current record.
+	 * @return The borrowBy date.
+	 */
+	public Calendar getBorrowByDate() {
+		return borrowBy;
+	}
+	
+	/**
+	 * This method returns the returnBy date of the current record.
+	 * @return The returnBy date.
+	 */
+	public Calendar getReturnByDate() {
+		return returnBy;
+	}
+}
---
+++ BookShelf/src/Publisher.java
@@ -0,0 +1,182 @@
+/**
+ * The Publisher class holds all the information related towards a book publishing company. 
+ * 
+ * @author Kevin Alexander
+ * @version May 31, 2014
+ *
+ */
+public class Publisher {
+	
+	/**
+	 * The publishing company's name.
+	 */
+	private String publisherName;
+	/**
+	 * The publishing company's street.
+	 */
+	private String publisherStreet;
+	/**
+	 * The publishing company's city.
+	 */
+	private String publisherCity;
+	/**
+	 * The publishing company's state.
+	 */
+	private String publisherState;
+	/**
+	 * The publishing company's ZipCode.
+	 */
+	private String publisherZip;
+	/**
+	 * The publishing company's country.
+	 */
+	private String publisherCountry;
+	/**
+	 * The year in which the publishing company was founded.
+	 */
+	private int publisherFounded;
+	
+	/**
+	 * The Publisher constructor. It takes in values for all its private fields and sets
+	 * them accordingly.
+	 * 
+	 * @param aName The publishing company's name.
+	 * @param aStreet The publishing company's street.
+	 * @param aCity The publishing company's city.
+	 * @param aZipCode The publishing company's ZipCode.
+	 * @param aState The publishing company's state.
+	 * @param aCountry The publishing company's country.
+	 * @param theFoundedYear The year in which the publishing company was founded.
+	 */
+	public Publisher(String aName, String aStreet, String aCity, String aZipCode, 
+					 String aState, String aCountry, int theFoundedYear) {
+		publisherName = aName;
+		publisherStreet = aStreet;
+		publisherCity = aCity;
+		publisherState = aState;
+		publisherCountry = aCountry;
+		publisherZip = aZipCode;
+		publisherFounded = theFoundedYear;
+	}
+	
+	/**
+	 * The Publisher constructor. It takes in no values and sets all string values to 
+	 * "Unknown" and the year founded to 0.
+	 */
+	public Publisher() {
+		this("Unknown", "Unknown", "Unknown", "Unknown", "Unknown", "Unknown", 0);
+	}
+	
+	/**
+	 * This method sets the name of the publishing company.
+	 * @param aName The new name of the publishing company.
+	 */
+	public void setName(String aName) {
+		publisherName = aName;
+	}
+	
+	/**
+	 * This method sets the street of the publishing company.
+	 * @param aStreet The new street of the publishing company.
+	 */
+	public void setStreet(String aStreet) {
+		publisherStreet = aStreet;
+	}
+	
+	/**
+	 * This method sets the city of the publishing company.
+	 * @param aCity The new city of the publishing company.
+	 */
+	public void setCity(String aCity) {
+		publisherCity = aCity;
+	}
+	
+	/**
+	 * This method sets the state of the publishing company.
+	 * @param aState The new state of the publishing company.
+	 */
+	public void setState(String aState) {
+		publisherState = aState;
+	}
+	
+	/**
+	 * This method sets the ZipCode of the publishing company.
+	 * @param aZipCode The new ZipCode of the publishing company.
+	 */
+	public void setZipCode(String aZipCode) {
+		publisherZip = aZipCode;
+	}
+	
+	/**
+	 * This method sets the country of the publishing company.
+	 * @param aCountry The new country of the publishing company.
+	 */
+	public void setCountry(String aCountry) {
+		publisherCountry = aCountry;
+	}
+	
+	/**
+	 * This method sets the year that the publishing company was founded.
+	 * @param theFoundedYear The new year that the publishing company was founded in.
+	 */
+	public void setFoundedYear(int theFoundedYear) {
+		publisherFounded = theFoundedYear;
+	}
+	
+	
+	/**
+	 * This method returns the publishing company's name.
+	 * @return The publishing company's name.
+	 */
+	public String getName() {
+		return publisherName;
+	}
+	
+	/**
+	 * This method returns the publishing company's street.
+	 * @return The publishing company's street.
+	 */
+	public String getStreet() {
+		return publisherStreet;
+	}
+	
+	/**
+	 * This method returns the publishing company's city.
+	 * @return The publishing company's city.
+	 */
+	public String getCity() {
+		return publisherCity;
+	}
+	
+	/**
+	 * This method returns the publishing company's state.
+	 * @return The publishing company's state.
+	 */
+	public String getState() {
+		return publisherState;
+	}
+	
+	/**
+	 * This method returns the publishing company's ZipCode.
+	 * @return The publishing company's ZipCode.
+	 */
+	public String getZipCode() {
+		return publisherZip;
+	}
+	
+	/**
+	 * This method returns the publishing company's country.
+	 * @return The publishing company's country.
+	 */
+	public String getCountry() {
+		return publisherCountry;
+	}
+	
+	/**
+	 * This method returns the year in which the publishing company was founded.
+	 * @return The year in which the publishing company was founded.
+	 */
+	public int getYearFounded() {
+		return publisherFounded;
+	}
+}
