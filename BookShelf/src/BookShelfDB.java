43cae4f100016be4711321b32689df8e5871b3fa
kmalex90@uw.edu <kmalex90@uw.edu@9ae6fbfb-7e5f-bf5b-6895-a5ad835334d6>
2014-06-02 18:20:48 +0000

    Added to the BookShelfDB - Kevin - 6/2/2014

--- BookShelf/src/BookShelfDB.java
+++ BookShelf/src/BookShelfDB.java
@@ -4,10 +4,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Date;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class BookShelfDB {
 	private static String userName = "root";
@@ -41,7 +44,7 @@ public class BookShelfDB {
 					 + ", publisherZip"
 					 + ", publisherCountry"
 					 + ", publisherFounded"
-					 + " from BookShelf.Publisher";
+					 + " from LibraryDB.Publisher";
 		
 		pubList = new ArrayList<Publisher>();
 		try {
@@ -70,5 +73,75 @@ public class BookShelfDB {
 			}
 		}
 		return pubList;
+	}
+	
+	public List<Publisher> getPublisherByName(String publisherName) {
+		List<Publisher> filterList = new ArrayList<Publisher>();
+		try {
+			pubList = getPublishers();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		for (Publisher pub : pubList) {
+			if (pub.getName().toLowerCase().contains(publisherName.toLowerCase())) {
+				filterList.add(pub);
+			}
+		}
+		
+		return filterList;
+	}
+	
+	/// other publisher sql statements
+	
+	public List<PatronRecord> getPatronRecords() throws SQLException {
+		if (conn == null) {
+			createConnection();
+		}
+		
+		Statement stmt = null;
+		String query = "select "
+					 + "recordID"
+					 + ", patronID"
+					 + ", bookID"
+					 + ", borrowBy"
+					 + ", returnBy"
+					 + " from LibraryDB.PatronRecord";
+		
+		prList = new ArrayList<PatronRecord>();
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while(rs.next()) {
+				int recordID = rs.getInt("recordID");
+				int patronID = rs.getInt("patronID");
+				int bookID = rs.getInt("bookID");
+				Date borrBy = rs.getDate("borrowBy");
+				Date retBy = rs.getDate("returnBy");
+				
+				Calendar borrowBy = new GregorianCalendar();
+				borrowBy.setTime(borrBy);
+				
+				Calendar returnBy = null;
+				
+				if (retBy != null) {
+					returnBy = new GregorianCalendar();
+					returnBy.setTime(retBy);
+				}
+				
+				PatronRecord pr = new PatronRecord(recordID, patronID, bookID, borrowBy, returnBy);
+				
+				prList.add(pr);
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+		
+		return prList;		 
 	}
 }
